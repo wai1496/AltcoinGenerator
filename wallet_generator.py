c3e656f406991547c176fa60fd4df77658a733cd
Javier López <m@javier.io>
2018-03-12 00:34:42 -0600

    - Add wallet_generator.py

---
+++ wallet_generator.py
@@ -0,0 +1,67 @@
+#!/usr/bin/env python
+#initially taken from https://davanum.wordpress.com/2014/03/17/generating-a-bitcoin-private-key-and-address/
+
+import ecdsa
+import ecdsa.der
+import ecdsa.util
+import hashlib
+import os
+import re
+import struct
+
+b58 = '123456789ABCDEFGHJKLMNPQRSTUVWXYZabcdefghijkmnopqrstuvwxyz'
+
+def base58encode(n):
+    result = ''
+    while n > 0:
+        result = b58[n%58] + result
+        n /= 58
+    return result
+
+def base256decode(s):
+    result = 0
+    for c in s:
+        result = result * 256 + ord(c)
+    return result
+
+def countLeadingChars(s, ch):
+    count = 0
+    for c in s:
+        if c == ch:
+            count += 1
+        else:
+            break
+    return count
+
+# https://en.bitcoin.it/wiki/Base58Check_encoding
+def base58CheckEncode(version, payload):
+    s = chr(version) + payload
+    checksum = hashlib.sha256(hashlib.sha256(s).digest()).digest()[0:4]
+    result = s + checksum
+    leadingZeros = countLeadingChars(result, '\0')
+    return '1' * leadingZeros + base58encode(base256decode(result))
+
+def privateKeyToWif(key_hex):
+    return base58CheckEncode(0x80, key_hex.decode('hex'))
+
+def privateKeyToPublicKey(s):
+    sk = ecdsa.SigningKey.from_string(s.decode('hex'), curve=ecdsa.SECP256k1)
+    vk = sk.verifying_key
+    return ('\04' + sk.verifying_key.to_string()).encode('hex')
+
+def pubKeyToAddr(s):
+    ripemd160 = hashlib.new('ripemd160')
+    ripemd160.update(hashlib.sha256(s.decode('hex')).digest())
+    return base58CheckEncode(0, ripemd160.digest())
+
+def keyToAddr(s):
+    return pubKeyToAddr(privateKeyToPublicKey(s))
+
+# Generate a random private key
+private_key = os.urandom(32).encode('hex')
+
+# You can verify the values on http://brainwallet.org/
+print "Secret Exponent (Uncompressed) : %s " % private_key
+print "Private Key : %s " % privateKeyToWif(private_key)
+print "Public  Key : %s " % privateKeyToPublicKey(private_key)
+print "Address     : %s " % keyToAddr(private_key)
